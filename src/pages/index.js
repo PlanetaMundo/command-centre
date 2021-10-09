f9bb1cabfdca1a4175db19ce8c9f801c245953e9
PlanetaMundo <83719285+PlanetaMundo@users.noreply.github.com>
2021-10-09 17:37:59 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,12 +21,12 @@ export default function IndexPage() {
 			{Current status: ${status ? 'on' : 'off'}}
             </button> */}
 			<h1>Hello.</h1>
-/*
+
 		<h2>This site is done by a good mate whose mission is to educate the population.</h2>
 	  <h2>Check my blog posts! Thanks. below is an irrelevant image</h2>
          
-//<img src={techImage} className={homeStyles.image} alt="TechImage"></img>
-*/
+</*img src={techImage} className={homeStyles.image} alt="TechImage"></img*/>
+
 <h2 className={homeStyles.topicsCovered}>Topics covered</h2>
 <div className={homeStyles.flexBox}>
 <h2 className={homeStyles.flexElement}>Linux</h2>
