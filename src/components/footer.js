35e497d730177fba0ea898f617215caa61f64a88
PlanetaMundo <83719285+PlanetaMundo@users.noreply.github.com>
2021-10-30 12:12:30 +0000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { graphql, useStaticQuery } from "gatsby"
 import * as footerStyles from './footer.module.scss'
+import githubLogo from './icons8-github-30.png'
 
 export default function Footer() {
     const data = useStaticQuery(graphql`
@@ -16,7 +17,8 @@ export default function Footer() {
         <footer className={footerStyles.footer}>
     {/*obtained from https://github.com/PlanetaMundo/command-centre/blob/main/gatsby-config.js*/}
             <p>Created by {data.site.siteMetadata.author}, © 2021 </p>
-
+            <img src={githubLogo} className={homeStyles.image} alt="logo"></img>
         </footer>
     )
 }
+icons8-github-30.png
