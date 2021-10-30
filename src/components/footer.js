e78ea1ae3ab03d1ec87ef10abd45bb871327a60e
PlanetaMundo <83719285+PlanetaMundo@users.noreply.github.com>
2021-10-30 12:28:11 +0000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,8 +16,10 @@ export default function Footer() {
     return (
         <footer className={footerStyles.footer}>
     {/*obtained from https://github.com/PlanetaMundo/command-centre/blob/main/gatsby-config.js*/}
-            <p>Created by {data.site.siteMetadata.author}, © 2021 </p>
-            <img src={githubLogo} className={footerStyles.image} alt="logo"></img>
+            <p>Created by {data.site.siteMetadata.author}, © 2021 
+            <a href="https://github.com/PlanetaMundo/command-centre">
+            <img src={githubLogo} className={footerStyles.image} alt="logo"></img></a>
+                </p>
         </footer>
     )
 }
